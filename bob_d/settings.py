a4b6f058ff370b1090c5c89563e24fa382fa0c16
leemk <pallang719@naver.com>
2023-09-22 22:44:02 +0900

    accoutapp push test

--- bob_d/settings.py
+++ bob_d/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountapp.apps.AccountappConfig',
+    'accountapp',
 ]
 
 MIDDLEWARE = [
