45ecb78d20344959ee218bd365175d52918ff3bf
leemk <pallang719@naver.com>
2023-09-23 02:30:24 +0900

    signupp create

--- bob_d/settings.py
+++ bob_d/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accountapp',
+    'signupapp',
 ]
 
 MIDDLEWARE = [
--- bob_d/urls.py
+++ bob_d/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    #path('',)디폴트인 경우 지정해야함
     path('admin/', admin.site.urls),
+    path('signup/',include('signupapp.urls')),
 ]
